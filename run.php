e08bb78dd3bf7f04aeebf1cd23321539283662aa
Marco Bonetti <marco@cruncher.ch>
2024-03-17 15:02:34 +0100

    Autobuild

--- run.php
+++ run.php
@@ -27,10 +27,16 @@ $headhtml = '<!DOCTYPE html><html><head>
         </div>
         <div id="leftbar" class="hidden">
             <div class="list">
-                <a href="index.html" class="homelinks halfit">Home</a><a onClick="window.location.reload()" class="homelinks halfit">Refresh</a><a href="saved.html" class="homelinks halfit">Saved</a><a href="subreddit.html?r=popular" class="homelinks halfit">Popular</a>
-                <div id="subscribed">Subscribe to subreddits to view them here.</div>        <div id="cthemew"><a href="themes.html" id="themebtn">Themes</a>  <a href="settings.html">Settings</a>  <a href="https://www.buymeacoffee.com/overdevsapps" rel="nofollow">Donate</a> <a href="new.html" rel="nofollow">New</a> <a href="contact.html" >Feedback</a> <br>
-                <small><span style="color:var(--lightc);">*</span>  New settings: Infinite Scrolling and Compact Mode.</small>
-              </div></div>
+                <a href="index.html" class="homelinks halfit">Home</a><a href="all.html" class="homelinks halfit">/r/all</a><a href="saved.html" class="homelinks halfit">Saved</a><a href="subreddit.html?r=popular" class="homelinks halfit">Popular</a>
+                <div id="subscribed">Subscribe to subreddits to view them here.</div>
+                <div id="cthemew">
+                    <a href="themes.html" id="themebtn">Themes</a>
+                    <a href="settings.html">Settings</a>
+                    <a href="https://www.buymeacoffee.com/overdevsapps" rel="nofollow">Donate</a>
+                    <a href="new.html" rel="nofollow">New</a> <a href="contact.html" >Feedback</a> <br>
+                        <small><span style="color:var(--lightc);">*</span>  New settings: Infinite Scrolling and Compact Mode.</small>
+                </div>
+            </div>
         </div>
         <div id="rightbar" class="hidden"></div>
     </div>
