0a4e3a490ac757c954da42e3c2a590beeb343028
Marco Bonetti <marco@cruncher.ch>
2026-03-25 14:09:25 +0100

    toggle width

--- js/functions.js
+++ js/functions.js
@@ -1151,6 +1151,33 @@ export function setTitle(title) {
     document.title = title + ' • RDX'
 }
 
+export function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+export function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+export function eraseCookie(name) {
+    document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+
+if(getCookie('wide') === '1' && window.innerWidth >= 800) {
+    document.body.classList.add('wide');
+}
+
 
 export function collapseComment(c){
     let thisp = c;
@@ -1351,6 +1378,18 @@ addEventListener("DOMContentLoaded", (event) => {
     document.getElementById('menu').addEventListener('click', (e) => {
         e.preventDefault();
         toggle('rightbar');
+    });
+
+
+
+    document.getElementById('toggle-width').addEventListener('click', (e) => {
+        e.preventDefault();
+        document.body.classList.toggle('wide');
+        if (document.body.classList.contains('wide')) {
+            setCookie('wide', '1', 365);
+        } else {
+            eraseCookie('wide');
+        }
     });
 
     document.getElementById('subssearchi').addEventListener('keyup', (e) => {
